932f9e311c316258c679fa4ee9e755688369089c
JPeck567 <190055002@aston.ac.uk>
2020-11-18 21:35:03 +0000

    Added mvp proj files

---
+++ PitchBlackProject/desktop/src/net/pitchblack/getenjoyment/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.pitchblack.getenjoyment.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.pitchblack.getenjoyment.PitchBlack;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PitchBlack(), config);
+	}
+}
