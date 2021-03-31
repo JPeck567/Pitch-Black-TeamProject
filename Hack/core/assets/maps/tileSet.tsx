684a3ff1bd4a77625b6532239ece898a03a60c41
Jorge <61948082+JPeck567@users.noreply.github.com>
2021-03-31 17:56:42 +0100

    Cleared hack from master branch

--- Hack/core/assets/maps/tileSet.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.10.30" name="tileSet" tilewidth="32" tileheight="32" tilecount="2" columns="2">
- <image source="tiles.jpg" width="64" height="32"/>
- <tile id="1">
-  <properties>
-   <property name="blocked" value=""/>
-  </properties>
- </tile>
-</tileset>
